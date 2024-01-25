00e1e556b15b32d0d898ef04371f07a7aef0c61c
Vivek Pratap <vivekpratapvp11@gmail.com>
2024-01-25 20:47:52 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const db = require("./database/db");
+
+
+app.use(express.json());
+app.use(cors());
+
+db.connect();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/profile", profileRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+  );
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
